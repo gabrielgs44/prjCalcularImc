ac4c7b7d6421661aa936e85b287f40e21abe55c3
Lorran Oliveira <lorran963@hotmail.com>
2019-05-22 22:24:25 -0300

    Modificando classe Programa

--- pesoImc/src/main/java/control/Programa.java
+++ pesoImc/src/main/java/control/Programa.java
@@ -8,6 +8,8 @@ public class Programa {
         Scanner leia = new Scanner(System.in);
         
         char sexo;
+        double imc = 0;
+        String resultado = null;
                
         do {       
         System.out.print("Digite o seu sexo (F OU M): ");
@@ -29,12 +31,21 @@ public class Programa {
         System.out.print("Digite a sua altura: ");
         double altura = leia.nextDouble();
         
-        ControlePessoa contpessoa = new ControlePessoa();
+        /*ControlePessoa contpessoa = new ControlePessoa();
         contpessoa.inserirPessoa(altura, peso, idade, sexo);
         double imc = contpessoa.obterPessoa().calcularImc(peso, altura);
         System.out.println("Seu imc: " + imc);
-        System.out.println("Você está: " + contpessoa.obterPessoa().calcularResultado(imc));
+        System.out.println("Você está: " + contpessoa.obterPessoa().calcularResultado(imc));*/
         
+        if(idade < 21 && sexo == 'M') {
+        	ControleCriancaMasculino contCriancaMas = new ControleCriancaMasculino();
+        	contCriancaMas.InserirCriancaMasculino(altura, peso, idade, sexo);
+        	imc = contCriancaMas.obterCriancaMasculino().calcularImc(peso, altura);
+        	resultado = contCriancaMas.criMasc.calcularPercentil(imc, idade);
+        }
+        
+        System.out.println("Seu imc: " + imc);
+        System.out.println("Você está: " + resultado);
     }
 
 }
